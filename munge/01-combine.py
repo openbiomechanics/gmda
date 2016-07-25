fd83881f0f7cd7f6e2359367b90715c20dff917d
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-07-25 16:49:26 -0500

    added munge script

--- munge/01-combine.py
+++ munge/01-combine.py
@@ -15,12 +15,32 @@ def main():
     # Load data
     print("Started!")
     data_dir = os.path.abspath('../data/original')
+    data_dir = os.path.abspath('./data/original')
     zipfile_name = 'gait-maturation-db.tar.gz'
     file = os.path.join(data_dir, zipfile_name)
     data = pd.DataFrame()
+    column_names = ['time', 'stride_interval']
+    with tarfile.open(file) as tar:
+        for member in tar.getmembers():
+            if '.txt' in member.name:
+                f = tar.extractfile(member)
+                x = f.read().decode('utf-8')
+                content = StringIO(x)
+                df = pd.read_csv(content, sep="\t", names=column_names)
+                name = member.name
+                name = name.replace('data/', '')
+                name = name.replace('.txt', '')
+                import re
+                x = re.match('([0-9]+)-([0-9]+)', name)
+                df['subject_id'] = int(x.groups()[0])
+                data = pd.concat([data, df], axis=0)
+
+    data = data[['subject_id'] + column_names]
+    data = data.sort_values(['subject_id', 'time'])
 
     # Write out data
-    data.to_csv('', index=False)
+    data.to_csv('../data/prepped/intervals.csv', index=False)
+    data.to_csv('./data/prepped/intervals.csv', index=False)
     print("Finished!")
 
 
