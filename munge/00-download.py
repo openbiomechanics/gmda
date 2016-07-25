323a56b87fa63c600d8523ab0c303fdcc560b5f7
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-07-25 16:34:08 -0500

    added download script

---
+++ munge/00-download.py
@@ -0,0 +1,19 @@
+"""TO BE EDITED.
+
+TO BE EDITED.
+
+Available defs:
+- f: TO BE EDITED.
+"""
+
+
+def main():
+    # Load data
+    print("Started!")
+
+    # Write out data
+    print("Finished!")
+
+
+if __name__ == '__main__':
+    main()
