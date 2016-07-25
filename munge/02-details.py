fa5b15f6aad35e7f6dd01141c4ce05d1a091df09
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-07-25 18:03:14 -0500

    added script to handle details about subjects

---
+++ munge/02-details.py
@@ -0,0 +1,36 @@
+"""TO BE EDITED.
+
+TO BE EDITED.
+
+Available defs:
+- f: TO BE EDITED.
+"""
+import pandas as pd
+import os
+
+
+def main():
+    # Load data
+    print("Started!")
+    data_dir = os.path.abspath('../data/original')
+    file = os.path.join(data_dir, 'details.csv')
+    data = pd.read_csv(file)
+
+    # Clean up column names
+    column_names = data.columns
+    column_names = [s.replace('(1-50)', '') for s in column_names]
+    column_names = [s.replace('/', '_per_') for s in column_names]
+    column_names = [s.replace('-', '_') for s in column_names]
+    column_names = [s.lower() for s in column_names]
+    data.columns = column_names
+
+    # Clean up data.frame
+
+    # Write out data
+    data.to_csv('../data/prepped/details.csv', index=False)
+
+    print("Finished!")
+
+
+if __name__ == '__main__':
+    main()
